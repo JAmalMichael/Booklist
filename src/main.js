b24be30fc4be34da92dbf78b29b2ffa97914580e
JAmal Michael <eshimi090@gmail.com>
2024-05-25 13:46:44 -1200

    functionality complete. without storage

--- src/main.js
+++ src/main.js
@@ -37,11 +37,45 @@ class UI {
             <td class="text-sm border-b-black border border-r-0 border-l-0">${anime.title}</td>
             <td class="text-sm border-b-black border border-r-0 border-l-0">${anime.genre}</td>
             <td class="text-sm border-b-black border border-r-0 border-l-0">${anime.main_act}</td>
-            <td><a href="#" class="text-sm text-red-500"><i class="fa-solid fa-xmark"></i> </a></td>
+            <td><a href="#" class="text-sm text-red-500 delete font-bold"> X </a></td>
             `;
 
             list.appendChild(row); 
            }
+
+           static clearFields() {
+            document.querySelector('#title').value = '';
+            document.querySelector('#genre').value = '';
+            document.querySelector('#main-act').value = '';
+           }
+
+           static deleteAnime(el) {
+            if( el.classList.contains('delete')) {
+                el.parentElement.parentElement.remove();
+            }
+           }
+
+          static showAlert(danger, className) {
+            const div = document.createElement('div');
+            div.className = `bg-red-500 w-4/5 p-1 text-white text-sm text-center mx-auto alert`;
+            div.appendChild(document.createTextNode(danger));
+            const container = document.querySelector('.section');
+            const form = document.querySelector('#anime-form');
+            container.insertBefore(div, form);
+
+            setTimeout(()=> document.querySelector('.alert').remove(), 3000);
+         } 
+
+          static showAlert2(success, className) {
+            const div = document.createElement('div');
+            div.className = `bg-green-500 w-4/5 p-1 text-white text-sm text-center mx-auto alert`;
+            div.appendChild(document.createTextNode(success));
+            const container = document.querySelector('.section');
+            const form = document.querySelector('#anime-form');
+            container.insertBefore(div, form);
+
+            setTimeout(()=> document.querySelector('.alert').remove(), 3000);
+         } 
 
 }
 
@@ -57,8 +91,25 @@ document.querySelector('#anime-form').addEventListener('submit', (e) => {
     const main_act = document.querySelector('#main-act').value;
 
 
-    // instantiating the anime
+    if(title == '' || main_act === '' || genre === '' ) {
+        UI.showAlert('Please fill in all fields', 'danger');
+    } else {
+           // instantiating the anime
      const anime = new Anime(title, genre, main_act);
 
-    UI.addAnimeToList(anime);
+     UI.addAnimeToList(anime);
+
+     UI.showAlert2('Anime Added', 'success');
+    
+     UI.clearFields();
+    }
+
+ 
 });
+
+document.querySelector('#anime-list').addEventListener('click', (e) => {
+    UI.deleteAnime(e.target);
+
+
+    UI.showAlert2('Anime Removed', 'success');
+})
